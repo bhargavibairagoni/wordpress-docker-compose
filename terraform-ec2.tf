a7c58854dbbfa6767f7a88ff282571eef57bbacb
Bhargavi Bairagoni <113205911+bhargavibairagoni@users.noreply.github.com>
2024-12-10 22:49:14 +0530

    Update terraform-ec2.tf

--- terraform-ec2.tf
+++ terraform-ec2.tf
@@ -1,3 +1,17 @@
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+
 resource "aws_security_group" "demosg" {
 
 ingress{
